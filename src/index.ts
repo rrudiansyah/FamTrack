680e9a778c9fbd44304c191be1ed534792b03e5b
Rahmad Rudiansyah <rrudiansyahsiregar@gmail.com>
2026-03-25 04:57:43 +0700

    chore: initial repository structure

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-import { Elysia } from 'elysia';
-
-const app = new Elysia()
-  .get('/', () => 'Hello dari Elysia')
-  .listen(3000);
-
-console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-);
